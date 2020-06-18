b7e1e2bfc57290f9512ed5a07cd8962cc4b4a1e8
César Rodríguez <cesar.rrguez@gmail.com>
2020-06-18 19:54:17 +0200

    Add Bootstrap Infrastructure layer

---
+++ App.Infra.Bootstrap/IoC.cs
@@ -0,0 +1,22 @@
+﻿using App.Application.Services;
+using App.Domain.Interfaces.Repositories;
+using App.Domain.Interfaces.Services;
+using App.Infra.Data;
+using SimpleInjector;
+
+namespace App.Infra.Bootstrap
+{
+    public class IoC
+    {
+        public static Container Container;
+
+        static IoC()
+        {
+            Container = new Container();
+
+            Container.Register<IProductService, ProductService>();
+
+            Container.Register<IProductRepository, ProductRepository>();
+        }
+    }
+}
