7927cce2a232764f034c8c3e9f7920da023307a6
César Rodríguez <cesar.rrguez@gmail.com>
2020-06-18 17:50:22 +0200

    Add Application layer

---
+++ App.Application/Services/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using App.Domain.Entities;
+using App.Domain.Interfaces.Repositories;
+using App.Domain.Interfaces.Services;
+
+namespace App.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public void RegisterNewProduct(Product product)
+        {
+            _productRepository.Add(product);
+        }
+    }
+}
