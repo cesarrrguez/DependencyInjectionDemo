e1e8d82c8432776df3996ac32a33a8720ed975af
César Rodríguez <cesar.rrguez@gmail.com>
2020-06-18 18:00:27 +0200

    Add Infrastructure layer

---
+++ App.Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using App.Domain.Entities;
+using App.Domain.Interfaces.Repositories;
+
+namespace App.Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine($"Adding product {product.Id} - {product.Description} into db");
+        }
+    }
+}
